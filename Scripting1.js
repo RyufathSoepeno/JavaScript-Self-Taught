63fd3bf79af4a6032c01299727b08dc1f16dfda6
ryu0210 <89006072+ryu0210@users.noreply.github.com>
2023-02-14 11:59:21 +0700

    Add files via upload

---
+++ Scripting1.js
@@ -0,0 +1,10 @@
+/** Your First Seperate JS code */
+
+// using console log
+console.log("Hello World (LOG)"); /** To run console.log, you need to have node.js extension */
+
+// using alert
+alert("Hello, World! (ALERT)");
+
+// using document write
+document.write("Hello, World! (Doc)");
---
+++ Scripting2(Variables&Let).js
@@ -0,0 +1,35 @@
+/** Variables are containers for storing data (storing data values) || IT CAN STORE ANY DATA TYPE */
+var x = 5;
+
+var y;
+y = 6;
+
+var code = "Delta";
+
+var z = x + y;
+
+document.write("<h3>" + code + " is " + x + " + " + y + "= " + z + "<br>");
+
+
+
+/** 'let' are variables annot be Redeclared with block scope */
+
+let p = 4;
+
+document.write("She is " + p);
+
+
+/* let p = "John Doe";
+
+let p = 0;
+
+// SyntaxError: 'x' has already been declared */
+
+
+
+/** const are variables Redeclared nor reassigned, but properties could change like objects in class, etc. */
+
+/* const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+PI = PI + 10;   // This will also give an error 
+*/
